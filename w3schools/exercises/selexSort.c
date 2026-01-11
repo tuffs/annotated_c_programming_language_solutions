e9e9c6a820522a6c51b9d874eca0b5ecfabdd3a6
Devon Kiss <devon@3dmandt.com>
2026-01-11 07:08:36 -0600

    Updates to the selection sort study.

--- w3schools/exercises/selexSort.c
+++ w3schools/exercises/selexSort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void selectionSort(int arr[], int n) {
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n - 1; i++) {
     int minIndex = i;
 
     for (int j = i + 1; j < n; j++) {
@@ -14,27 +14,27 @@ void selectionSort(int arr[], int n) {
         arr[i] = arr[minIndex];
         arr[minIndex] = temp;
       }
-    }
+    } 
   }
 }
 
 int main() {
-  int myArr[] = {500,250,100,0,-100,-250,-500}; 
+  int myArr[] = {200,100,0,-100,-200,-300};
   int arrLength = sizeof(myArr)/sizeof(myArr[0]);
 
   selectionSort(myArr, arrLength);
 
-  for (int i = 0; i < arrLength; i++) {
-    if (i == 0) {
+  for (int x = 0; x < arrLength; x++) {
+    if (x == 0) {
       printf("[");
     }
 
-    printf("%d", myArr[i]);
+    printf("%d", myArr[x]);
 
-    if (i == arrLength - 1) {
-      printf("]\n");
-    } else {
+    if (x < arrLength - 1) {
       printf(", ");
+    } else {
+      printf("]\n");
     }
   }
 
